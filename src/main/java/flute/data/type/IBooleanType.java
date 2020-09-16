ad334d09e311f0d0c24460e22e89cde8dc95d6d9
maytinhdibo <iammaytinhdibo@gmail.com>
2020-09-16 17:05:38 +0700

    add new token

--- src/main/java/flute/data/type/IBooleanType.java
+++ src/main/java/flute/data/type/IBooleanType.java
@@ -8,8 +8,7 @@ import java.util.List;
 public class IBooleanType extends IGenericType {
     @Override
     public boolean canBeAssignmentBy(ITypeBinding iTypeBinding) {
-        List<String> boolType = Arrays.asList(new String[]{"Ljava/lang/Boolean;", "Z"});
-        if (boolType.contains(iTypeBinding.getKey())) {
+        if (TypeConstraintKey.BOOL_TYPES.contains(iTypeBinding.getKey())) {
             return true;
         }
         return false;
