8b392b16317135f1bcabf22e725a3014ef599a4b
maytinhdibo <iammaytinhdibo@gmail.com>
2020-10-24 20:11:26 +0700

    DFG throw in catch

--- src/main/java/flute/jdtparser/DFGParser.java
+++ src/main/java/flute/jdtparser/DFGParser.java
@@ -96,7 +96,8 @@ public class DFGParser {
             if (!isInitialized(variableName, tryStatement.getBody(), curPos)) return false;
             for (Object catchObject : tryStatement.catchClauses()) {
                 if (catchObject instanceof CatchClause) {
-                    if (!isInitialized(variableName, ((CatchClause) catchObject).getBody(), curPos)) {
+                    if (!isInitialized(variableName, ((CatchClause) catchObject).getBody(), curPos)
+                            && !checkThrowInCatchBlock(((CatchClause) catchObject).getBody())) {
                         return false;
                     }
                 }
@@ -126,6 +127,13 @@ public class DFGParser {
             }
         }
 
+        return false;
+    }
+
+    public static boolean checkThrowInCatchBlock(Block catchBlock) {
+        for (Object statement : catchBlock.statements()) {
+            if (statement instanceof ThrowStatement) return true;
+        }
         return false;
     }
 
