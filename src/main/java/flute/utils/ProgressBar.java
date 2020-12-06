44cfc69599d13bf5f9741a7147f12545c290134f
maytinhdibo <iammaytinhdibo@gmail.com>
2020-12-06 23:48:03 +0700

    update ProgressBar auto print

--- src/main/java/flute/utils/ProgressBar.java
+++ src/main/java/flute/utils/ProgressBar.java
@@ -1,6 +1,31 @@
 package flute.utils;
 
+import flute.config.Config;
+import flute.utils.logging.Timer;
+
 public class ProgressBar {
+    private Timer timer;
+    private float progress;
+    private long eta;
+
+    public ProgressBar() {
+        timer = new Timer();
+    }
+
+    public void setProgress(float progress, boolean print) {
+        if (print && (this.progress - progress) > Config.PRINT_PROGRESS_DELTA) {
+            long runTime = Timer.getCurrentTime().getTime() - timer.getLastTime().getTime();
+            eta = (long) ((runTime / progress) - runTime) / 1000;
+            System.out.printf("%5.2f %s - ETA: %s\n",
+                    progress * 100f, genProgressBar(progress * 100, Config.PROGRESS_SIZE), Timer.formatTime(eta));
+        }
+        this.progress = progress;
+    }
+
+    public void setProgress(float progress) {
+        setProgress(progress, false);
+    }
+
     public static void printProcessBar(float percent, int size) {
         System.out.print(genProgressBar(percent, size));
     }
