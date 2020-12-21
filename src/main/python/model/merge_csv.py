ca66de5a86b84d0bb5cbbe5ad68db29a04298c46
Minology <kientt312@gmail.com>
2020-12-21 16:15:27 +0700

    add n-fold creation and preprocess from existing excode and java

--- src/main/python/model/merge_csv.py
+++ src/main/python/model/merge_csv.py
@@ -3,20 +3,23 @@ import pandas as pd
 from pathlib import Path
 
 # proj = ['ant', 'batik', 'log4j', 'lucene', 'xalan', 'xerces']
-proj = ['netbeans', 'eclipse']
-typ = ['excode', 'java']
-dt = ['train', 'test', 'validate']
-data_parent_folders = ['data_csv_6_gram']
+projects = ['netbeans', 'eclipse']
+data_forms = ['java', 'excode']
+# data_types = ['train', 'test', 'validate']
+data_types = ['fold_' + str(x) for x in range(10)]
+data_parent_folders = ['data_csv_5_gram', 'data_csv_7_gram']
+data_version = '3'
 
-for sub_typ in typ:
-    for sub_dt in dt:
+for data_form in data_forms:
+    for data_type in data_types:
         for data_parent_folder in data_parent_folders:
             all_filenames = []
-            for sub_proj in proj:
-                all_filenames.append('../../../../../' + data_parent_folder + '/' + sub_typ + '/' + sub_proj + '/' + sub_typ + '_' +
-                                     sub_dt + "_" + sub_proj + '.csv')
+            for project in projects:
+                all_filenames.append('../../../../../data_v' + data_version + '/' + data_parent_folder + '/' +
+                                     data_form + '/' + project + '/' + data_form + '_' + data_type + "_" + project + '.csv')
             print(all_filenames)
             combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
-            Path('../../../../../' + data_parent_folder + '/' + sub_typ + '/all/').mkdir(parents=True, exist_ok=True)
-            combined_csv.to_csv('../../../../../' + data_parent_folder + '/' + sub_typ + '/all/' + sub_typ + '_' +
-                                sub_dt + '_all.csv', index=False, encoding='utf-8-sig')
+            Path('../../../../../data_v' + data_version + '/' + data_parent_folder + '/' + data_form + '/all/')\
+                .mkdir(parents=True, exist_ok=True)
+            combined_csv.to_csv('../../../../../data_v' + data_version + '/' + data_parent_folder + '/' + data_form +
+                                '/all/' + data_form + '_' + data_type + '_all' + '.csv', index=False, encoding='utf-8-sig')
