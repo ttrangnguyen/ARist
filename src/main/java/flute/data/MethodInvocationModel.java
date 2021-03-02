3f8f7d3ae9bb263c23d79ecfe6b5a57f9c9282d3
maytinhdibo <iammaytinhdibo@gmail.com>
2021-03-02 23:01:40 +0700

    fix null binding

--- src/main/java/flute/data/MethodInvocationModel.java
+++ src/main/java/flute/data/MethodInvocationModel.java
@@ -35,7 +35,7 @@ public class MethodInvocationModel {
         orgASTNode = superMethodInvocation;
         methodBinding = superMethodInvocation.resolveMethodBinding();
         expression = superMethodInvocation;
-        expressionType = superMethodInvocation.resolveMethodBinding().getDeclaringClass();
+        expressionType = methodBinding == null ? null : methodBinding.getDeclaringClass();
         arguments = superMethodInvocation.arguments();
         genArgumentTypes();
         methodName = superMethodInvocation.getName();
