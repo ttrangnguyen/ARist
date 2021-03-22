9cb8c1198dcaa62f6a007821d7af76a2e41f77f2
maytinhdibo <iammaytinhdibo@gmail.com>
2021-03-22 10:54:20 +0700

    add swt and swing crawler

--- src/main/java/flute/crawling/APICrawler.java
+++ src/main/java/flute/crawling/APICrawler.java
@@ -41,10 +41,48 @@ public class APICrawler {
         throw new Exception("Can not found");
     }
 
+    public static List<String> awtswingLink(String classString, String methodString) throws Exception {
+        String url = "https://docs.oracle.com/javase/8/docs/api/"
+                + classString.replace(".", "/") + ".html";
+        Document document = Jsoup.connect(url).get();
+        Element methodTable = document.select("a[name=\"method.summary\"]").first()
+                .parent().select(".memberSummary").first();
+
+        Elements rows = methodTable.select("tr[id]");
+
+        for (Element row : rows) {
+            String method = row.select(".colLast").first().select("code").first().text();
+            String paramString = method.substring(method.indexOf("(") + 1, method.length() - 1);
+            String methodNameString = method.substring(0, method.indexOf("("));
+            List<String> paramTypes = new ArrayList<>();
+            List<String> paramNames = new ArrayList<>();
+            Arrays.stream(paramString.split(", ")).forEach(param -> {
+                if (param.length() > 0) {
+                    paramTypes.add(param.split(" ")[0]);
+                    paramNames.add(param.split(" ")[1]);
+                }
+            });
+            String methodStringGen = methodNameString + "(" + String.join(",", paramTypes) + ")";
+
+            if (methodString.equals(methodStringGen)) {
+                return paramNames;
+            }
+        }
+        throw new Exception("Can not found");
+    }
+
+    public static List<String> paramNames(String classString, String methodString) throws Exception {
+        if (classString.startsWith("org.eclipse.swt.")) {
+            return swtLink(classString, methodString);
+        } else if (classString.startsWith("java.awt.") || classString.startsWith("javax.swing.")) {
+            return awtswingLink(classString, methodString);
+        }
+        throw new Exception("Can not found");
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println(
-                swtLink("org.eclipse.swt.widgets.Display", "asyncExec(Runnable)")
-        );
+        System.out.println(swtLink("org.eclipse.swt.widgets.Display", "asyncExec(Runnable)"));
+        System.out.println(awtswingLink("java.awt.AWTEventMulticaster", "add(ActionListener,ActionListener)"));
     }
 
 }
