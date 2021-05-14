4bfad15d6f9c3e72331e369fc5d5abb467bd13f0
maytinhdibo <iammaytinhdibo@gmail.com>
2021-05-14 22:32:46 +0700

    fix APITestGenerator

--- src/test/java/flute/jdtparser/APITestGenerator.java
+++ src/test/java/flute/jdtparser/APITestGenerator.java
@@ -2,13 +2,17 @@ package flute.jdtparser;
 
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import flute.data.MultiMap;
 import flute.data.testcase.BaseTestCase;
 import flute.data.testcase.Candidate;
 import flute.utils.file_processing.FileProcessor;
-import flute.utils.logging.Logger;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -16,34 +20,58 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class APITestGenerator {
     public static final String REPO_FOLDER = "storage/repositories/git/JAVA_repos/";
     public static final String INPUT_FOLDER = "/Users/maytinhdibo/Downloads/generated_test_cases/";
+    public static final String OUTPUT_FOLDER = "storage/logs/out/";
 
-    public static void main(String[] args) {
-        File inputFolder = new File(INPUT_FOLDER);
-        Gson gson = new Gson();
-        for (File project : inputFolder.listFiles()) {
-            //read input
-            AtomicInteger numberOfTest = new AtomicInteger();
-            List<String> inputs = FileProcessor.readLineByLineToList(project.getAbsolutePath() + "/parameter_name_test_cases.jsonl");
-            inputs.forEach(input -> {
-                BaseTestCase testCase = gson.fromJson(input, BaseTestCase.class);
-                try {
-                    MultiMap result = APITest.test(testCase);
-                    List<Candidate> candidates = new ArrayList<>();
-                    result.getValue().forEach((excode, value) -> {
-                        value.forEach(lexCandidate -> {
-                                    candidates.add(new Candidate(excode, lexCandidate));
-                                }
-                        );
-                    });
+    public static final String PROJECT_NAME = "3breadt_dd-plist";
 
-                    testCase.setCandidates(candidates);
-                    Logger.write(gson.toJson(testCase), "/out/" + project.getName() + "/parameter_name_candidates.jsonl");
-                    numberOfTest.getAndIncrement();
-                } catch (Exception e) {
-//                    e.printStackTrace();
-                }
-            });
-            System.out.println("[RESULT] --------------[Generated " + numberOfTest + " tests]--------------");
+    public static void main(String[] args) throws IOException {
+//        File inputFolder = new File(INPUT_FOLDER);
+        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+        ;
+
+        File project = new File(INPUT_FOLDER);
+//        for (File project : inputFolder.listFiles()) {
+        //read input
+        AtomicInteger numberOfTest = new AtomicInteger();
+        AtomicInteger numberOfErrorTest = new AtomicInteger();
+        List<String> inputs = FileProcessor.readLineByLineToList(
+                Paths.get(INPUT_FOLDER, PROJECT_NAME, "parameter_name_test_cases.jsonl").toString());
+
+        File outFile = new File(Paths.get(OUTPUT_FOLDER, PROJECT_NAME, "parameter_name_candidates.jsonl").toString());
+        if (!outFile.exists()) {
+            outFile.getParentFile().mkdirs();
+        } else {
+            outFile.delete();
         }
+        outFile.createNewFile();
+
+        FileWriter fstream = new FileWriter(outFile.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fstream);
+
+        inputs.forEach(input -> {
+            BaseTestCase testCase = gson.fromJson(input, BaseTestCase.class);
+            try {
+                MultiMap result = APITest.test(testCase);
+                List<Candidate> candidates = new ArrayList<>();
+                result.getValue().forEach((excode, value) -> {
+                    value.forEach(lexCandidate -> {
+                                candidates.add(new Candidate(excode, lexCandidate));
+                            }
+                    );
+                });
+
+                testCase.setCandidates(candidates);
+                bw.write(gson.toJson(testCase));
+                bw.newLine();
+                numberOfTest.getAndIncrement();
+            } catch (Exception e) {
+//                    e.printStackTrace();
+                numberOfErrorTest.getAndIncrement();
+            }
+        });
+        System.out.println("[RESULT] --------------[Generated " + numberOfTest + " tests]--------------");
+        System.out.println("         --------------[Ignore " + numberOfErrorTest + " tests]--------------");
+        bw.close();
     }
+//    }
 }
