a94595567a15ef9493f5f21318f8593fcd722822
Trần Mạnh Cường <iammaytinhdibo@gmail.com>
2021-07-29 16:43:45 +0700

    return param name list

--- src/main/java/flute/jdtparser/utils/DevUtils.java
+++ src/main/java/flute/jdtparser/utils/DevUtils.java
@@ -1,19 +1,29 @@
-package flute.jdtparser.utils;
+package maytinhdibo.eclipse.jdt.core.dom;
 
 import org.eclipse.jdt.core.dom.IMethodBinding;
-import org.eclipse.jdt.internal.compiler.impl.ReferenceContext;
-import org.eclipse.jdt.internal.compiler.lookup.CompilationUnitScope;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DevUtils {
-    public static ReferenceContext getResolver(IMethodBinding methodBinding) throws Exception {
-        Class myClass = methodBinding.getClass();
-        Field resolver = myClass.getDeclaredField("resolver");
-        resolver.setAccessible(true);
+    public static List<String> getResolver(IMethodBinding methodBinding) throws Exception {
 
-        Field scope = resolver.get(methodBinding).getClass().getDeclaredField("scope");
-        scope.setAccessible(true);
-        return ((CompilationUnitScope) scope.get(resolver.get(methodBinding))).referenceContext().getCompilationUnitDeclaration();
+        Class myClass = methodBinding.getClass();
+        Field binding = myClass.getDeclaredField("binding");
+        binding.setAccessible(true);
+
+        Object bindingField = binding.get(methodBinding);
+
+        Class bindingClass = bindingField.getClass();
+        Field parameterNames = bindingClass.getDeclaredField("parameterNames");
+        parameterNames.setAccessible(true);
+
+        ArrayList<String> parameterNameList = new ArrayList<>();
+        char[][] paramNameArr = (char[][]) parameterNames.get(bindingField);
+        for (int i = 0; i < paramNameArr.length; i++) {
+            parameterNameList.add(String.valueOf(paramNameArr[i]));
+        }
+        return parameterNameList;
     }
 }
