09099924d984856ffc517cb4f89ea78db5dc8306
Minology <kientt312@gmail.com>
2021-08-09 18:23:03 +0700

    public static member

---
+++ src/main/java/flute/jdtparser/PublicStaticMember.java
@@ -0,0 +1,13 @@
+package flute.jdtparser;
+
+public class PublicStaticMember {
+    public String key;
+    public String excode;
+    public String lexical;
+
+    public PublicStaticMember(String key, String excode, String lexical) {
+        this.key = key;
+        this.excode = excode;
+        this.lexical = lexical;
+    }
+}
