24bb249859da8e4f01027b86cf2233bc50e97e54
maytinhdibo <iammaytinhdibo@gmail.com>
2021-10-03 17:37:14 +0700

    sample test stat

--- src/main/java/flute/jdtparser/statistics/PublicStaticReduce.java
+++ src/main/java/flute/jdtparser/statistics/PublicStaticReduce.java
@@ -2,23 +2,94 @@ package flute.jdtparser.statistics;
 
 import flute.config.Config;
 import flute.data.MultiMap;
+import flute.data.testcase.Candidate;
 import flute.jdtparser.FileParser;
 import flute.jdtparser.ProjectParser;
+import flute.jdtparser.statistics.ps.TestCase;
+import flute.testing.CandidateMatcher;
 import flute.utils.ProgressBar;
-import flute.utils.file_processing.DirProcessor;
+import flute.utils.file_processing.FileProcessor;
 import flute.utils.logging.Logger;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 public class PublicStaticReduce {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
+        List<String> lines = FileProcessor.readLineByLineToList("/Users/maytinhdibo/Project/Flute/storage/logs/sampletest.csv");
+        List<TestCase> testCases = new ArrayList<>();
+        lines.forEach(line -> {
+            String[] testData = line.split(",");
+            TestCase testCase = new TestCase(testData[0], Integer.valueOf(testData[1]), Integer.valueOf(testData[2]), Integer.valueOf(testData[3]));
+            testCases.add(testCase);
+        });
+
+        Config.loadConfig(Config.STORAGE_DIR + "/json/netbeans.json");
+        ProjectParser projectParser = new ProjectParser(Config.PROJECT_DIR, Config.SOURCE_PATH,
+                Config.ENCODE_SOURCE, Config.CLASS_PATH, Config.JDT_LEVEL, Config.JAVA_VERSION);
+        projectParser.loadPublicStaticMembers();
+        projectParser.loadPublicStaticRTMembers();
+        AtomicInteger id = new AtomicInteger();
+        testCases.forEach(testCase -> {
+            id.getAndIncrement();
+            File file = new File("storage/repositories/git/netbeans/" + testCase.getFilePath());
+            FileParser fileParser = new FileParser(projectParser, file, 0, 0);
+            try {
+                fileParser.setPosition(fileParser.getPosition(testCase.getLine(), testCase.getCol()) + 1);
+                fileParser.parse();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            MultiMap result = fileParser.genParamsAt(testCase.getArgPos());
+            AtomicBoolean isMatched = new AtomicBoolean(false);
+
+            String target = "";
+
+            try {
+                target = fileParser.getCurMethodInvocation().arguments().get(testCase.getArgPos()).toString();
+            } catch (Exception e) {
+                target = ")";
+            }
+
+            long numberOfCandidates = 0;
+            String finalTarget = target;
+            for (Map.Entry<String, List<String>> entry : result.getValue().entrySet()) {
+                numberOfCandidates = numberOfCandidates + entry.getValue().size();
+                entry.getValue().forEach(item -> {
+                    Logger.write(item, "testcase/testcase_" + id + ".txt");
+                    if (!isMatched.get() && CandidateMatcher.matches(new Candidate(entry.getKey(), item), finalTarget)) {
+                        isMatched.set(true);
+                    }
+                });
+            }
+            int origin = projectParser.getFasterPublicStaticCandidates(result.getParamTypeKey()).size();
+            int reduce = projectParser.getFasterPublicStaticCandidates(result.getParamTypeKey(), file.getPath()).size();
+            projectParser.getFasterPublicStaticCandidates(result.getParamTypeKey(), file.getPath()).forEach(item -> {
+                Logger.write(item.lexical, "testcase/testcase_" + id + ".txt");
+                if (!isMatched.get() && CandidateMatcher.matches(new Candidate(item.excode, item.lexical), finalTarget)) {
+                    isMatched.set(true);
+                }
+            });
+            Logger.write(String.format("%s,%d,%d,%d,%d,%d,%d,%b",
+                    testCase.getFilePath(),
+                    testCase.getLine(),
+                    testCase.getCol(),
+                    testCase.getArgPos(),
+                    numberOfCandidates,
+                    origin,
+                    reduce,
+                    isMatched.get()
+            ), "sampletest_run.csv");
+        });
+    }
+
+    public static void stat() throws IOException {
         Config.loadConfig(Config.STORAGE_DIR + "/json/netbeans.json");
         ProjectParser projectParser = new ProjectParser(Config.PROJECT_DIR, Config.SOURCE_PATH,
                 Config.ENCODE_SOURCE, Config.CLASS_PATH, Config.JDT_LEVEL, Config.JAVA_VERSION);
@@ -27,17 +98,23 @@ public class PublicStaticReduce {
         projectParser.loadPublicStaticRTMembers();
 
         //get list java files
-        List<File> allJavaFiles = DirProcessor.walkJavaFile(Config.PROJECT_DIR);
+//        List<File> allJavaFiles = DirProcessor.walkJavaFile(Config.PROJECT_DIR);
 
-        List<File> javaFiles = allJavaFiles.stream().filter(file -> {
-            if (!file.getAbsolutePath().contains("src")) return false;
+//        List<File> javaFiles = allJavaFiles.stream().filter(file -> {
+//            if (!file.getAbsolutePath().contains("src")) return false;
+//
+//            for (String blackName : Config.BLACKLIST_NAME_SRC) {
+//                if (file.getAbsolutePath().contains(blackName)) return false;
+//            }
+//
+//            return true;
+//        }).collect(Collectors.toList());
+        Set<File> javaFileSet = new HashSet<>();
 
-            for (String blackName : Config.BLACKLIST_NAME_SRC) {
-                if (file.getAbsolutePath().contains(blackName)) return false;
-            }
+        List<String> lines = FileProcessor.readLineByLineToList("/Users/maytinhdibo/Project/Flute/storage/logs/sampletest.csv");
+        List<File> javaFiles = new ArrayList<>();
 
-            return true;
-        }).collect(Collectors.toList());
+
         final int[] testCount = {0};
         AtomicInteger fileCount = new AtomicInteger();
         ProgressBar progressBar = new ProgressBar();
