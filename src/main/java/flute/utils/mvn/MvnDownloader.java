99f9d982b983c3adabe9b53c3c532a60d4e9efbc
NewLuminous <newluminous7@gmail.com>
2021-10-22 14:26:46 +0700

    Update MvnDownloader.java

--- src/main/java/flute/utils/mvn/MvnDownloader.java
+++ src/main/java/flute/utils/mvn/MvnDownloader.java
@@ -1,8 +1,11 @@
 package flute.utils.mvn;
 
+import flute.config.Config;
+import flute.utils.file_processing.DirProcessor;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.shared.invoker.*;
 import org.bouncycastle.math.raw.Mod;
 import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
@@ -14,6 +17,9 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MvnDownloader {
     public static void main(String[] args) throws IOException, XmlPullParserException {
@@ -85,5 +91,43 @@ public class MvnDownloader {
             }
         }
         return null;
+    }
+
+    /**
+     * Download jar from pom.xml
+     * @return 0 if successfully download all dependencies.
+     */
+    public static int download(String projectPath) {
+        AtomicInteger exitCode = new AtomicInteger();
+
+        DirProcessor.getAllEntity(new File(projectPath), false).stream().filter(file -> {
+            return file.getAbsolutePath().replace('\\','/').endsWith("/pom.xml");
+        }).forEach(pomFile -> {
+            InvocationRequest request = new DefaultInvocationRequest();
+            request.setPomFile(pomFile);
+            System.out.println("=======MAVEN IS BUILDING=======");
+            request.setOutputHandler(new InvocationOutputHandler() {
+                @Override
+                public void consumeLine(String s) {
+                    //do nothing
+                }
+            });
+            request.setGoals(Arrays.asList("dependency:copy-dependencies", "-DoutputDirectory=\"" +
+                    Paths.get(pomFile.getParentFile().getAbsolutePath(), "flute-mvn")
+                    + "\""));
+
+            Invoker invoker = new DefaultInvoker();
+            invoker.setMavenHome(new File(Config.MVN_HOME));
+            invoker.setInputStream(InputStream.nullInputStream());
+            try {
+                int statusCode = invoker.execute(request).getExitCode();
+                if (statusCode > exitCode.get()) {
+                    exitCode.set(statusCode);
+                }
+            } catch (MavenInvocationException e) {
+                e.printStackTrace();
+            }
+        });
+        return exitCode.get();
     }
 }
