25f14a59c9b7561e2d293e235d09a8c04037797e
Trần Trung Kiên <35617941+Minology@users.noreply.github.com>
2022-05-12 01:18:15 +0700

    Update missing files

---
+++ PEARL-local-model/src/main/java/flute/communicating/PublicStaticEfficiencyData.java
@@ -0,0 +1,11 @@
+package flute.communicating;
+
+public class PublicStaticEfficiencyData {
+    public int fold;
+    public Integer test_id;
+    public Integer total;
+    public Integer reduced;
+    public int containTargetBeforeReduced;
+    public int containTargetAfterReduced;
+    public String paramTypeKey;
+}
---
+++ PEARL-local-model/src/main/java/flute/communicating/RecentClassEfficiencyData.java
@@ -0,0 +1,9 @@
+package flute.communicating;
+
+public class RecentClassEfficiencyData {
+    public int fold;
+    public int test_id;
+    public int totalRecent;
+    public int containTarget;
+    public String paramTypeKey;
+}
